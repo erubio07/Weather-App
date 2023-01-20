55a8815a8cc67b10b96340d83f95934615d5df3d
Ezequiel <erubio07@gmail.com>
2023-01-20 16:15:29 -0300

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,11 @@ import React from "react";
 
 export default function About () {
     return <div>
-        Algo Sobre El Creador
+        <h1>Weather App</h1>
+        <h3>Aplicación disponible para consultar el estado del clima en tus ciudades favoritas</h3>
+        <h2>Sobre el Autor:</h2>
+        <p>Soy estudiante de la carrera Full Stack en Henry.
+        Weather App esta creada con React y es parte de un proyecto integrador de FrontEnd.
+        </p>
     </div>
 }
